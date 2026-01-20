1541f41ce729e5c8ca31ce2af1312a2b66bf2182
SetthuL <langesiphosethu@gmail.com>
2026-01-20 19:16:20 +0200

    Added Data in data.py

---
+++ data.py
@@ -0,0 +1,33 @@
+
+products =["Laptop",
+           "Smartphone", 
+           "Tablet", 
+           "Headphones", 
+           "Smartwatch", 
+           "Camera", 
+           "Printer", 
+           "Monitor", 
+           "Wireless Keyboard", 
+           "Wireless Mouse"]
+
+prices = [15000.99, 
+          6000.99, 
+          8000.99, 
+          450.99, 
+          1500.99, 
+          7390.99, 
+          17600.99, 
+          10000.99, 
+          1200.99, 
+          400.99]
+
+daily_sales = [150, 
+         300, 
+         200, 
+         95, 
+         62, 
+         180, 
+         220, 
+         160, 
+         500, 
+         600]
