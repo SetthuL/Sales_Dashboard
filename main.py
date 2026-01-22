7c69e427345e9aabcb4b1ea652d8a20ea167c65f
SetthuL <langesiphosethu@gmail.com>
2026-01-22 19:46:17 +0200

    Folder charts created to save charts images

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os
 import matplotlib.pyplot as plt
 from data import products, daily_sales
 
+def save_charts():
+    if not os.path.exists('charts'):
+        os.makedirs('charts')
+
+'''
 def calculate_daily_sales():
     products = []
     daily_sales = []
+'''
 
 def plot_daily_sales(products, daily_sales):
     plt.title("Daily Sales of Electronic Products")
@@ -12,6 +19,8 @@ def plot_daily_sales(products, daily_sales):
     plt.bar(products, daily_sales, color='skyblue')
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
+    
+    plt.savefig('charts/daily_sales.png')
     
     plt.show()
     
